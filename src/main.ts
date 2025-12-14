1b4356311afc7812efcc75c08da4230e3c620013
zihadulislam2 <zihadul708@gmail.com>
2025-12-14 07:13:27 +0600

    fix: Correct CORS origin URL by removing trailing slash

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://sahara-53.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://sahara-53.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
